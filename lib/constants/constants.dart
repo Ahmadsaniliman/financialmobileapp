40f4f1eb0940bb0dfa986e8d9809e6d61f3f1e9e
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-09-24 23:22:09 +0100

    another commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimary = Color(0xFFFF9255);
+const kSecondary = Color(0xFFFFFFFF);
+const kBackgroundColor = Color(0xFF262626);
+const kBottomColor = Color(0xFF303030);
---
+++ lib/onBoarding/on_boarding.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreenView extends StatefulWidget {
+  const OnBoardingScreenView({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingScreenView> createState() => _OnBoardingScreenViewState();
+}
+
+class _OnBoardingScreenViewState extends State<OnBoardingScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
